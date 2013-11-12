588670776b047c1c312c378da0746ae505c90493
Vikram Jambulapati <vikjam@gmail.com>
2013-11-11 22:45:12 -0500

    set up mech

--- fetch-trips.rb
+++ fetch-trips.rb
@@ -2,5 +2,14 @@
 
 require 'mechanize'
 
+def mech()
+    Mechanize.new do |agent|
+        agent.user_agent_alias = 'Mac Safari'
+        agent.follow_meta_refresh = true
+        agent.open_timeout = 60
+        agent.read_timeout = 60
+    end
+end
+
 # End of script
 
